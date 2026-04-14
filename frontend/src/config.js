808839e84c88b18ce22795ae1bb1889d00fe6f6d
m4rkss <marcgvarela@gmail.com>
2026-04-14 18:50:19 +0200

    dockerfile front

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 /** Base URL del backend (CRA inyecta REACT_APP_* en build). */
-export const API_URL = process.env.REACT_APP_BACKEND_URL;
+export const API_URL = "https://api.centralitamunicipal.com";
